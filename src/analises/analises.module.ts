46dd7768c8594f6f3ea6c9f1d229bd22e3886d19
G4BR13LR0CH4 <42114098+G4BR13LR0CH4@users.noreply.github.com>
2021-09-01 20:12:05 -0300

    Update analises.module.ts

--- src/analises/analises.module.ts
+++ src/analises/analises.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AnalisesController } from './analises.controller';
 import { AnalisesService } from './analises.service';
-import { AnaliseSchema } from './schemas/analises.schema';
+//import { AnaliseSchema } from './schemas/analises.schema';
 
 @Module({
   controllers: [AnalisesController],
   providers: [AnalisesService],
-  imports: [
+  /*imports: [
     MongooseModule.forFeature([{ name: 'Analises', schema: AnaliseSchema }])
-  ]
+  ]*/
 })
 export class AnalisesModule {}
